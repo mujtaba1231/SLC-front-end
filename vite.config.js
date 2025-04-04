e9ad8c5d459b91a378e0dcc80d473b32e2594dca
Mujtaba12-al <mujtaba@mativeinc.com>
2025-04-04 23:05:39 +0500

    Build and update frontend for production1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'dist', // Ensure this is correct, this is the default output directory
+    outDir: 'build', // Ensure this is correct, this is the default output directory
   },
   plugins: [react()],
 })
